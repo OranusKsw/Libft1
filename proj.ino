848dadb18eac3bc42cc079254c05ae3d5c158c73
OranusKsw <112540736+OranusKsw@users.noreply.github.com>
2023-05-17 16:36:37 +0700

    Update proj.ino

--- proj.ino
+++ proj.ino
@@ -6,11 +6,13 @@ int e = 4;  //For displaying segment "e"
 int f = 5;  //For displaying segment "f"
 int g = 6;  //For displaying segment "g"
 const int buttonPin[4] = {14, 15, 16, 17}; 
+int score = 0;
 
 int buttonState[4] = {0,0,0,0};
-   
+int randint;
 
-void setup() {               
+void setup() {         
+  Serial.begin(9600);      
   pinMode(a, OUTPUT);  //A
   pinMode(b, OUTPUT);  //B
   pinMode(c, OUTPUT);  //C
@@ -71,32 +73,59 @@ void turnOff()
 
 void buttonn()
 {
-    buttonState[1] = digitalRead(buttonPin[1]);
-  buttonState[2] = digitalRead(buttonPin[2]);
-  buttonState[3] = digitalRead(buttonPin[3]);
-  buttonState[0] = digitalRead(buttonPin[0]);
 
-  for(int i = 0; i < 4; i++)
+Serial.println("Here2");
+  while(digitalRead(14) == LOW && digitalRead(15) == LOW&& digitalRead(16) == LOW&& digitalRead(17) == LOW);
+  Serial.println("Here3");
+  for(int i = 14; i < 18; i++)
   {
-   if (buttonState[i] == HIGH) {
-    Serial.println(buttonPin[i]);
-    digitalWrite(buttonPin[i]-6, HIGH);
+   if (digitalRead(i) == HIGH)
+   {
+    if(i == randint + 6) {
+      //Serial.println(digitalRead(i));
+      digitalWrite(randint, LOW);
+      score++;
+      Serial.println(score);
+      displayDigit(score);
+     // while(digitalRead(i) == HIGH);
+      loop();
+    }
+    else
+    { 
+      Serial.println("i");
+    }
   } 
   else {
-    
-    digitalWrite(buttonPin[i]-6, LOW);
+    Serial.println(i);
   }
+  Serial.println("Here");
   }
+  buttonn();
 }
 
-void loop() {
 
-Serial.println(buttonPin[0]);
-buttonn();
- for(int i=0;i<10;i++)
+void displayseg()
+{
+   for(int i=0;i<10;i++)
  {
    displayDigit(i);
-   delay(1000);
    turnOff();
  }
+  }
+
+ void mole()
+ {
+  randint= random(8, 12);
+  digitalWrite(randint, HIGH);
+  
+  }
+void loop() {
+  for(int i = 8; i < 12; i++)
+  {
+    digitalWrite(i, LOW);  
+  }
+  mole();
+  buttonn();
+  Serial.println("Here1");
+  delay(1000);
 }
